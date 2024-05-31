14ffc06751ea27ca1e2591d04cc7a208d80c3fe2
Nico Swan <hi@nicoswan.com>
2024-05-31 21:13:38 +0200

    updated inital config

--- install.sh
+++ install.sh
@@ -41,22 +41,27 @@ sed -i "s|./hardware-configuration.nix|./hardware-configuration.nix ./boot.nix .
 sed -i "s|networking.useDHCP|# networking.useDHCP|g" /mnt/etc/nixos/hardware-configuration.nix
 
 # Set root password
-export rootPwd=$(mkpasswd -m SHA-512 -s "VerySecurePassword")
+export userPwd='$y$j9T$sZlZK2gaQO/GLQPMMjGDS1$uCF3JloZrwTzLsxZuAvkJrw6/Z6ls/jPbkJgO/EqQy1';
 # Write boot.nix configuration
 tee -a /mnt/etc/nixos/boot.nix <<EOF
 { config, pkgs, lib, ... }:
 {
 
+  # My Initrd config, enable ZSTD compression and use systemd-based stage 1 boot
+  boot.initrd = {
+    compressor = "zstd";
+    compressorArgs = [ "-19" "-T0" ];
+    systemd.enable = true;
+  };
 
-    boot.loader.grub = {
-      enable = true;
-      copyKernels = true;
-      zfsSupport = true;
-      efiSupport = true;
-      efiInstallAsRemovable = true;
-      mirroredBoots = [
-        { devices = [ "nodev" "$DISK" ]; path = "/boot"; }
-      ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    mirroredBoots = [
+      { devices = [ "nodev" "$DISK" ]; path = "/boot"; }
+    ];
   };
 }
 EOF
@@ -64,12 +69,13 @@ EOF
 tee -a /mnt/etc/nixos/users.nix <<EOF
 { config, pkgs, lib, ... }:
 {
-    users.users.root.initialHashedPassword = "$rootPwd";
+    users.mutableUsers = false;
+    users.users.root.initialHashedPassword = "$userPwd";
     users.users.nicoswan = {
           isNormalUser = true;
           description = "Nico Swan";
           extraGroups = [ "networkmanager" "wheel" ];
-
+          hashedPassword = "$userPwd";
           openssh.authorizedKeys.keys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJzDICPeNfXXLIEnf4FEQ5ZGX6REsNEPaeRbyxOh7vVL NicoMacLaptop"
           ];
@@ -88,13 +94,19 @@ tee -a /mnt/etc/nixos/networking.nix <<EOF
 { config, pkgs, lib, ... }:
 {
     
-    networking.useDHCP = lib.mkDefault false;
-    networking.interfaces.ens18.ipv4.addresses = [{
-       address = "102.135.163.95";
-       prefixLength = 24;
-    }];
-    networking.defaultGateway = "102.135.163.1";
-    networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  # Disable NixOS's builtin firewall
+  networking.firewall.enable = false;
+
+  # Hostname, can be set as you wish
+  networking.hostName = "vm403bfeq";
+
+  networking.useDHCP = lib.mkDefault false;
+  networking.interfaces.ens18.ipv4.addresses = [{
+    address = "102.135.163.95";
+    prefixLength = 24;
+  }];
+  networking.defaultGateway = "102.135.163.1";
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
 }
 EOF
@@ -128,6 +140,16 @@ tee -a /mnt/etc/nixos/nix-config.nix <<EOF
     '';
   };
 
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    btop
+  ];
 }
 EOF
 
